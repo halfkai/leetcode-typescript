ae3251aabd3304a9aa11bb2cff0ed6501bf4d557
halfkai <0x158105@gmail.com>
2026-04-13 23:20:36 +0800

    problems (0002): add test cases

--- problems/0002-add-two-numbers/solution.test.ts
+++ problems/0002-add-two-numbers/solution.test.ts
@@ -5,20 +5,20 @@ import { addTwoNumbersArray } from "./solution";
 
 describe("addTwoNumbersArray", () => {
   test("example 1", () => {
-    const result = addTwoNumbersArray([2,4,3], [5,6,4]);
+    const result = addTwoNumbersArray([2, 4, 3], [5, 6, 4]);
     // TODO: replace with expected output
-    expect(result).toEqual(result satisfies unknown);
+    expect(result).toEqual([7, 0, 8]);
   });
 
   test("example 2", () => {
     const result = addTwoNumbersArray([0], [0]);
     // TODO: replace with expected output
-    expect(result).toEqual(result satisfies unknown);
+    expect(result).toEqual([0]);
   });
 
   test("example 3", () => {
-    const result = addTwoNumbersArray([9,9,9,9,9,9,9], [9,9,9,9]);
+    const result = addTwoNumbersArray([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9]);
     // TODO: replace with expected output
-    expect(result).toEqual(result satisfies unknown);
+    expect(result).toEqual([8, 9, 9, 9, 0, 0, 0, 1]);
   });
 });
