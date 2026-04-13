ae74445009a8a0e501702b6bfc98023f36286c0f
halfkai <0x158105@gmail.com>
2026-04-13 23:27:31 +0800

    problems (0003): add test cases and explain the code

--- problems/0003-longest-substring-without-repeating-characters/solution.test.ts
+++ problems/0003-longest-substring-without-repeating-characters/solution.test.ts
@@ -7,18 +7,18 @@ describe("lengthOfLongestSubstring", () => {
   test("example 1", () => {
     const result = lengthOfLongestSubstring("abcabcbb");
     // TODO: replace with expected output
-    expect(result).toEqual(result satisfies number);
+    expect(result).toEqual(3);
   });
 
   test("example 2", () => {
     const result = lengthOfLongestSubstring("bbbbb");
     // TODO: replace with expected output
-    expect(result).toEqual(result satisfies number);
+    expect(result).toEqual(1);
   });
 
   test("example 3", () => {
     const result = lengthOfLongestSubstring("pwwkew");
     // TODO: replace with expected output
-    expect(result).toEqual(result satisfies number);
+    expect(result).toEqual(3);
   });
 });
--- problems/0003-longest-substring-without-repeating-characters/solution.ts
+++ problems/0003-longest-substring-without-repeating-characters/solution.ts
@@ -1,10 +1,16 @@
 export function lengthOfLongestSubstring(s: string): number {
-    const map = new Map();
-    let max = 0;
-    for (let l = 0, r = 0; r < s.length;) {
-        if (typeof map.get(s[r]) === 'number') {
-            
-        }
-        map.set(s[r], r);
+  const map = new Map();
+  let max = 0;
+  for (let l = 0, r = 0; r < s.length; r++) {
+    const idx = map.get(s[r]);
+    if (typeof idx === "number" && idx >= l) {
+      // l represents the boundary of the current window
+      // if idx >= l meaning that the repeated character is inside the current window
+      // so we need to move the left pointer to the next index of the repeated character
+      l = idx + 1;
     }
-};
+    max = Math.max(max, r - l + 1);
+    map.set(s[r], r);
+  }
+  return max;
+}
