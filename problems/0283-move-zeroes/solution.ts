fb0fb4726963021081b6d985f03dafe98e2f2060
halfkai <0x158105@gmail.com>
2026-05-19 20:44:41 +0800

    probs (0283): reduce swap times

--- problems/0283-move-zeroes/solution.ts
+++ problems/0283-move-zeroes/solution.ts
@@ -15,16 +15,18 @@ export function moveZeroes(nums: number[]): number[] {
     }
     nums.push(...Array.from<number>({ length }).fill(0))
     
-    console.log(moveZeroes);
     return nums;
 }
 
 export function moveZeroesTwoPointers(nums: number[]): void {
     for (let i = 0, j = 0; i < nums.length; i++) {
         if (nums[i] !== 0) {
-            const tmp = nums[i]!;
-            nums[i] = nums[j]!;
-            nums[j++] = tmp;
+            if (i !== j) {
+                const tmp = nums[i]!;
+                nums[i] = nums[j]!;
+                nums[j] = tmp;
+            }
+            j++;
         }
     }
 }
