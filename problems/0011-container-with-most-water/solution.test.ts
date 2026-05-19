47550d587db74fc08085a00da46b208665efca35
halfkai <0x158105@gmail.com>
2026-05-19 14:37:44 +0800

    probs (0011): done

--- problems/0011-container-with-most-water/solution.test.ts
+++ problems/0011-container-with-most-water/solution.test.ts
@@ -7,12 +7,12 @@ describe("maxArea", () => {
   test("example 1", () => {
     const result = maxArea([1,8,6,2,5,4,8,3,7]);
     // TODO: replace with expected output
-    expect(result).toEqual(result satisfies number);
+    expect(result).toEqual(49);
   });
 
   test("example 2", () => {
     const result = maxArea([1,1]);
     // TODO: replace with expected output
-    expect(result).toEqual(result satisfies number);
+    expect(result).toEqual(1);
   });
 });
--- problems/0011-container-with-most-water/solution.ts
+++ problems/0011-container-with-most-water/solution.ts
@@ -1,3 +1,15 @@
 export function maxArea(height: number[]): number {
-    
+    let l = 0;
+    let r = height.length - 1;
+    let max = 0;
+    while (l < r) {
+        // calculating the area with minium height, then compare with previous stored value.
+        max = Math.max((r - l) * Math.min(height[l]!, height[r]!), max);
+        if (height[l]! < height[r]!) {
+            l++;
+        } else {
+            r--;
+        }
+    }
+    return max;
 }
