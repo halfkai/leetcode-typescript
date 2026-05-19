7277dfc41d2696dcf29d3fad423fa88dbcb42d2f
halfkai <0x158105@gmail.com>
2026-05-19 20:44:55 +0800

    probs (0015): add test code

--- problems/0015-3sum/solution.test.ts
+++ problems/0015-3sum/solution.test.ts
@@ -7,18 +7,18 @@ describe("threeSum", () => {
   test("example 1", () => {
     const result = threeSum([-1,0,1,2,-1,-4]);
     // TODO: replace with expected output
-    expect(result).toEqual(result satisfies unknown);
+    expect(result).toEqual([[-1,-1,2],[-1,0,1]]);
   });
 
   test("example 2", () => {
     const result = threeSum([0,1,1]);
     // TODO: replace with expected output
-    expect(result).toEqual(result satisfies unknown);
+    expect(result).toEqual([]);
   });
 
   test("example 3", () => {
     const result = threeSum([0,0,0]);
     // TODO: replace with expected output
-    expect(result).toEqual(result satisfies unknown);
+    expect(result).toEqual([[0,0,0]]);
   });
 });
